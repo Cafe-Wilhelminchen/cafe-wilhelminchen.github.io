a83c5a334f8bbe1f729461dc1eebb0f824609f92
Luis Hankel <kaes3kuch3n@gmail.com>
2022-09-22 17:13:58 +0200

    Added proxy for calendar access

--- themes/cafe/static/js/event-calendar.js
+++ themes/cafe/static/js/event-calendar.js
@@ -1,10 +1,7 @@
-const CALENDAR_API_KEY = "AIzaSyDhMuN1rKcFmVVAK4eSGww5vMpyu5MYRj0";
-const CALENDAR_ID = "fb8pavce2k8cvbh0uisluh8c04@group.calendar.google.com";
-
 document.addEventListener("DOMContentLoaded", init);
 
 async function init() {
-    const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${CALENDAR_API_KEY}`);
+    const response = await fetch(`https://cafe.students-htw.de/calendar-api/`);
     const data = await response.json();
     console.log(data);
 }
